293818c78e301122b9336aaa225888a4fbfb5fb8
Marcel Klehr <mklehr@gmx.net>
2023-06-23 16:09:40 +0200

    llm.py: Search for models dir in current directory

--- llm.py
+++ llm.py
@@ -1,3 +1,5 @@
 from langchain.llms import GPT4All
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-llm = GPT4All(model="./models/ggml-gpt4all-j-v1.3-groovy.bin", n_ctx=512, n_threads=6)
+llm = GPT4All(model=dir_path + "/models/ggml-gpt4all-j-v1.3-groovy.bin", n_ctx=512, n_threads=6)
