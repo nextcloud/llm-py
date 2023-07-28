8d1da106b216519ab1dd02da499ef440486a687c
Marcel Klehr <mklehr@gmx.net>
2023-07-28 11:10:42 +0200

    Use falcon model

--- llm.py
+++ llm.py
@@ -4,4 +4,4 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 threads = os.environ.get('LLM_THREADS', 4)
 
-llm = GPT4All(model=dir_path + "/models/ggml-gpt4all-j-v1.3-groovy.bin", n_ctx=512, n_threads=int(threads))
+llm = GPT4All(model=dir_path + "/models/ggml-model-gpt4all-falcon-q4_0.bin", n_threads=int(threads))
