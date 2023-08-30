d65e3cf7132e99266b8c12eb85fe9f914c4b2079
Marcel Klehr <mklehr@gmx.net>
2023-08-30 12:48:06 +0200

    Allow switching model

--- index.py
+++ index.py
@@ -1,13 +1,26 @@
 import argparse
-
+import time
 from chains.formalize import FormalizeChain
 from chains.simplify import SimplifyChain
 from chains.summarize import SummarizeChain
 from chains.headline import HeadlineChain
 from chains.topics import TopicsChain
 from chains.free_prompt import FreePromptChain
-from llm import llm
 import sys
+from langchain.llms import GPT4All
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--task", choices=['summarize', 'simplify', 'formalize', 'headline', 'topics', 'free_prompt'], default='simplify')
+parser.add_argument("-f", "--file", default='./test/fixtures/article.txt')
+parser.add_argument("--text")
+parser.add_argument("--model", default='llama-2-7b-chat.ggmlv3.q4_0.bin')
+
+args = parser.parse_args()
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+threads = os.environ.get('LLM_THREADS', 4)
+llm = GPT4All(model=dir_path + "/models/"+args.model, n_threads=int(threads))
 
 chains = {}
 chains['summarize'] = SummarizeChain(llm=llm)
@@ -16,13 +29,6 @@ chains['formalize'] = FormalizeChain(llm=llm)
 chains['headline'] = HeadlineChain(llm=llm)
 chains['topics'] = TopicsChain(llm=llm)
 chains['free_prompt'] = FreePromptChain(llm=llm)
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--task", choices=chains.keys(), default='summarize')
-parser.add_argument("-f", "--file", default='./test/fixtures/meeting2.txt')
-parser.add_argument("--text")
-
-args = parser.parse_args()
 
 if args.text:
     if args.text != '-':
--- llm.py
+++
@@ -1,7 +0,0 @@
-from langchain.llms import GPT4All
-import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-threads = os.environ.get('LLM_THREADS', 4)
-
-llm = GPT4All(model=dir_path + "/models/ggml-model-gpt4all-falcon-q4_0.bin", n_threads=int(threads))
